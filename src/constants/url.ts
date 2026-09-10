24ffb7197a4c87f3abe2db7e83001e0ec449c23e
Fzansa <faizanansari8747@gmail.com>
2026-09-10 11:32:56 +0530

    image url issue fixed

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -7,4 +7,4 @@ export const API_URL =
   isLocalhost ? "http://localhost:5200/api/v1" : "https://ifma-api.esta-dev.com/api/v1";
 
 export const FILE_URL =
-  isLocalhost ? "http://localhost:5200/uploads" : "https://ifma-api.esta-dev.com/api/v1/uploads";
+  isLocalhost ? "http://localhost:5200/uploads" : "https://ifma-api.esta-dev.com/uploads";
