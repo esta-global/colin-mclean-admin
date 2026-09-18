9727346a4a1f354203e2bbcf5a97326ad306caa7
Hamid Raza <hamidrazaansari22@gmail.com>
2026-09-19 01:00:10 +0530

    first commit

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,7 +4,7 @@ const isLocalhost = hostname === "localhost" || hostname === "127.0.0.1";
 export const BASE_URL = "http://localhost:5173";
 
 export const API_URL =
-  isLocalhost ? "http://localhost:5201/api/v1" : "https://colin-mclean-api.esta-dev.com//api/v1";
+  isLocalhost ? "http://localhost:5201/api/v1" : "https://colin-mclean-api.esta-dev.com/api/v1";
 
 export const FILE_URL =
-  isLocalhost ? "http://localhost:5201/uploads" : "https://colin-mclean-api.esta-dev.com//uploads";
+  isLocalhost ? "http://localhost:5201/uploads" : "https://colin-mclean-api.esta-dev.com/uploads";
